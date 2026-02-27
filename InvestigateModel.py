447257548d37e76c18d912c9800c14f96d788d61
ZeynepDelalMutlu <zeynepdelalmutlu@gmail.com>
2026-02-27 09:42:52 +0100

    comments are edited

--- InvestigateModel.py
+++ InvestigateModel.py
@@ -1,23 +1,19 @@
 import onnxruntime as ort
 
-# Modeli yükleyelim
+# load model
 try:
     session = ort.InferenceSession("adv_inception_v3_Opset16.onnx")
-    
-    print("--- MODEL ANALİZ SONUÇLARI ---")
-    
-    # Giriş Kapısı Bilgileri
+        
     for input_node in session.get_inputs():
-        print(f"Giriş Kapısı Adı: {input_node.name}")
-        print(f"Beklenen Şekil (Shape): {input_node.shape}")
-        print(f"Veri Tipi: {input_node.type}")
+        print(f"model name: {input_node.name}")
+        print(f"image name: {input_node.shape}")
+        print(f"data type: {input_node.type}")
     
     print("-" * 30)
     
-    # Çıkış Kapısı Bilgileri
     for output_node in session.get_outputs():
-        print(f"Çıkış Kapısı Adı: {output_node.name}")
-        print(f"Çıkış Şekli: {output_node.shape}")
+        print(f"output name: {output_node.name}")
+        print(f"output shape: {output_node.shape}")
 
 except Exception as e:
-    print(f"Model dosyası bulunamadı veya bir hata oluştu: {e}")
+    print(f"Error: {e}")
